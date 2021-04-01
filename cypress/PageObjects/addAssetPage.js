3b31191ebc7bbe681437134cdb4f4e23a64f812f
Interviewer <81255185+z-interviewer@users.noreply.github.com>
2021-04-02 00:32:41 +0200

    Update addAssetPage.js

--- cypress/PageObjects/addAssetPage.js
+++ cypress/PageObjects/addAssetPage.js
@@ -1,28 +1,28 @@
 class addAsset {
 
-getAssetName() {
-    return cy.get('#defaultFormAddAsset');
-}
+    getAssetName() {
+        return cy.get('#defaultFormAddAsset');
+    }
 
-getSendAsset() {
-    return cy.contains('Send');
-}
+    getSendAsset() {
+        return cy.contains('Send');
+    }
 
-getValidAsset(){
-    return cy.get('.valid-feedback');
-}
+    getValidAsset(){
+        return cy.get('.valid-feedback');
+    }
 
-getAssetButtonLabel(){
-    return cy.get('.grey-text');
-}
+    getAssetButtonLabel(){
+        return cy.get('.grey-text');
+    }
 
-getAddAsset(){
-    return cy.get('a[href*="#/add"]');
-}
+    getAddAsset(){
+        return cy.get('a[href*="#/add"]');
+    }
 
-getInvalidAsset(){
-    return cy.get('.invalid-feedback');
-}
+    getInvalidAsset(){
+        return cy.get('.invalid-feedback');
+    }
 
 }
 export default addAsset
