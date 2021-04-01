e347ef2e29abd6e27e34bf9fcce4f2c903943f13
Interviewer <81255185+z-interviewer@users.noreply.github.com>
2021-04-02 00:33:05 +0200

    Update descriptionPage.js

--- cypress/PageObjects/descriptionPage.js
+++ cypress/PageObjects/descriptionPage.js
@@ -1,10 +1,8 @@
 class description {
 
-getDescription() {
-    //return cy.get('.grey-text').invoke("text").should("eq", 'New Asset');;
-    //return cy.get('a[href*="#/add"]');
-    return cy.contains('Description');
-}
+    getDescription() {
+        return cy.contains('Description');
+    }
 
 }
 export default description
